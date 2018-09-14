c80f410fc9993d9d1e46b1239d9bfe78a01a92e3
yashchoubey <yashchoubey2009@gmail.com>
2018-09-15 00:04:49 +0530

    methods updated

--- mozioApp/ServiceArea/views.py
+++ mozioApp/ServiceArea/views.py
@@ -4,8 +4,6 @@ from django.http import HttpResponse
 from ServiceArea.models import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from django.shortcuts import render
-# Create your views here.
 
 class Services_Providers(APIView):
 	def get(self,request):
@@ -15,7 +13,6 @@ class Services_Providers(APIView):
 			'email' : x.email,
 			'phone' : x.phone,
 			'language' : x.language,
-			# 'currency' : x.currency
 		} for x in providerObjects],status=200)
 
 	def put(self,request):
@@ -36,20 +33,27 @@ class Services_Providers(APIView):
 			name = request.data['name'],
 			email = request.data['email'],
 			phone = request.data['phone'],
-			language = request.data['language'],
-			# currency = request.data['currency']
+			language = request.data['language']
 		)
 		return Response(status=200)
 
 
 class Update_Service_Area(APIView):
+	#Method to make a new entry
 	def post(self,request):
 		providerObj = Provider.objects.get(id=request.data['id'])
-		providerObj.service_area=data['service_area']
-		providerObj.save()
-	
+		areaObj = Area.objects.get_or_create(latitude=request.data['latitude'],longitude=request.data['longitude'],currency=request.data['currency'])[0]
+		providerAreaObj = ProviderArea.objects.get_or_create(provider_id=providerObj,area_id=areaObj)
+		return Response(status=200)
+
+	#Method to fetch all service areas for existing provider
 	def get(self,request):
 		providerObj = Provider.objects.get(id=request.data['id'])
-		return Response(json.dumps(provider.service_area), status=200)
+		providerAreaObjs = ProviderArea.objects.filter(provider_id=providerObj)
+		return Response([{
+			'latitude':x.latitude,
+			'longitude':x.longitude,
+			'currency':x.currency
+		} for x in providerAreaObjs ], status=200)
 
 	
